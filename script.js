28e22ba24113bde928113c6740564439576bef0b
susanamontanini <lunaplateadadelface@gmail.com>
2023-06-02 17:29:05 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showAlert () { alert('Gracias, mensaje enviado satisfactoriamente.');
+}
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")
+                    }
+                    )
